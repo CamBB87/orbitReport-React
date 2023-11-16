db60933635dc064c71641fe01e4e391da5c50307
Patryk Pietrzak <cameron.beebe87@yahoo.com>
2023-11-15 22:17:12 -0600

    the assignments version

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import satData from "./components/satData.jsx";
 
 function App() {
   const [sat, setSat] = useState(satData);
-  const displaySats = [...new Set(satData.map((data) => data.data.orbitType))];
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
 
   const filterByType = (currentType) => {
     const displaySats = satData.filter((newSatDisplay) => {
-      return newSatDisplay.data.orbitType === currentType;
+      return newSatDisplay.orbitType === currentType;
     });
     setSat(displaySats);
   };
